574d0fa36abacc3b1bc9e344c4f302ee7eabc126
ceress <markmesina69@gmail.com>
2020-06-23 12:23:11 -0700

    create html page

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.use(express.static('public'));
 //socket middleware
 const io = socket(server);
 io.on('connection', (socket) => {
-    console.log('socket connection initialized')
+    console.log('socket connection initialized', socket.id)
 });
