4ab2bb557add67e800ff915960bc98953adc5b23
ceress <markmesina69@gmail.com>
2020-06-23 12:15:50 -0700

    init socket io connection

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const socket = require('socket.io');
 
 // App set up
-
 const app = express();
-const PORT = 8080
+const PORT = 8080;
 const server = app.listen(PORT, function() {
-    console.log (`App running on server ${PORT}`)
+    console.log (`App running on PORT ${PORT}`)
 });
 
-// static files
-
+// static files middleware
 app.use(express.static('public'));
+
+//socket middleware
+const io = socket(server);
+io.on('connection', (socket) => {
+    console.log('socket connection initialized')
+});
