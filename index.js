38ec102e4e5cb9667cca291b4f147e3afdc6795e
ceress <markmesina69@gmail.com>
2020-06-23 12:42:40 -0700

    init basic app functionality

--- index.js
+++ index.js
@@ -14,5 +14,9 @@ app.use(express.static('public'));
 //socket middleware
 const io = socket(server);
 io.on('connection', (socket) => {
-    console.log('socket connection initialized', socket.id)
+    console.log('socket connection initialized', socket.id);
+
+    socket.on('chat', (data) => {
+        io.sockets.emit('chat', data);
+    })
 });
--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,23 @@
 // make connection
 const socket = io.connect('http://localhost:8080');
+
+//query DOM
+const message = document.getElementById('message');
+const handle = document.getElementById('handle');
+const btn = document.getElementById('send');
+const output = document.getElementById('output');
+
+//event
+btn.addEventListener('click', () =>{
+    //takes 2 params message and cb ghandle
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    });
+});
+
+// listen for event
+socket.on('chat', (data)=> {
+    output.innerHTML += '<p><strong>' + data.handle + ':</strong>' 
+    + data.message + '</p>'
+})
